7d2b22649e00038422c08b1b36b66341db27e505
VaagnDavtyan <78389280+VaagnDavtyan@users.noreply.github.com>
2021-06-09 13:23:37 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import "./styles.css";
+
+class App extends Component {
+constructor(props){
+super(props);
+
+this.state={
+newItem:"",
+list:[]
+}
+}
+
+
+updateInput(key, value) {
+  this.setState({
+    [key]:value
+  })
+}
+
+addItem() {
+const newItem ={
+id: 1 + Math.random(),
+value: this.state.newItem.slice()
+}
+
+const list = [...this.state.list];
+
+list.push(newItem);
+
+this.setState({ list, newItem:"" })
+}
+
+
+deleteItem(id) {
+  
+  const list = [...this.state.list];
+  
+  const updatedList = list.filter(item => item.id !== id);
+
+  this.setState({ list: updatedList });
+}
+
+
+render() {
+  return (
+<div className="App">
+<div>
+<h1>Add an Item...</h1>
+<br/>
+<input
+type="text"
+placeholder="Type item here..."
+value={this.state.newItem}
+onChange={e => this.updateInput("newItem", e.target.value)}
+/>
+
+
+
+<button className = 'addbtn' onClick={() => this.addItem()}>Add Item</button>
+<br/>
+<ul>
+{this.state.list.map(item => {
+  return (
+<li key={item.id}>{item.value}
+<button className = 'deletebtn' onClick = {() => this.deleteItem(item.id)}>Delete</button>
+</li>
+  )
+})}
+</ul>
+</div>
+</div>
+  )
+}
+}
+export default App;
